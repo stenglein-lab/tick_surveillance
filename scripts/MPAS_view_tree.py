0953144c0dc4a985db10f46aed2929bfca9cc24b
Lynn Osikowicz <lynnno86@gmail.com>
2022-11-23 08:16:28 -0700

    changed ML tool from FastTree to IQ-Tree in main.nf, updated conda env file, updated MPAS_view_tree.py script, cleaned up ref seq names in targets.tsv

--- scripts/MPAS_view_tree.py
+++ scripts/MPAS_view_tree.py
@@ -9,8 +9,9 @@ import sys
 
 # setup command one line arguments:
 
-fasttree: sys.argv[1]
-
+iqtree: sys.argv[1]
+no_exten = os.path.splitext(sys.argv[1])
+out_name = no_exten[0]
 # create tree pdf with toytree python tool. Documentation found here: https://toytree.readthedocs.io/en/latest/
 tree = toytree.tree(sys.argv[1])
 colorlist = ['purple' if 'seq' in tip else 'black' for tip in tree.get_tip_labels()]
@@ -20,9 +21,9 @@ canvas, axes, makr = tree.draw(
         tip_labels_colors=colorlist,
 	height=1000,
         width = 1000,
-        node_hover=True, 
-        node_sizes=5, 
-        node_colors='teal', 
+        node_hover=True,
+        node_sizes=5,
+        node_colors='teal',
         tree_style='n',
         scalebar=True);
-toyplot.pdf.render(canvas, f'{sys.argv[1]}.pdf')
+toyplot.pdf.render(canvas, f'{out_name}.pdf')
